b733ca02bca415e3d51a5bc4798c03f37490d5c1
bartoszszmit37 <41542962+bartoszszmit37@users.noreply.github.com>
2019-01-27 13:00:59 +0100

    Adding next c-sharp files.

---
+++ SunSetAnimationWinFormTechnology/SunSetAnimation/Form1.Designer.cs
@@ -0,0 +1,57 @@
+﻿namespace animacja_ukladu_slonecznego
+{
+    partial class SunSetSpace
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.bs_ZegarUkladuSlonecznego = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // bs_ZegarUkladuSlonecznego
+            // 
+            this.bs_ZegarUkladuSlonecznego.Enabled = true;
+            this.bs_ZegarUkladuSlonecznego.Tick += new System.EventHandler(this.bs_ZegarUkladuSlonecznego_Tick);
+            // 
+            // PrzestrzenUkladuSlonecznego
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(644, 322);
+            this.Name = "PrzestrzenUkladuSlonecznego";
+            this.Text = "Animacja układu słonecznego";
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.PrzestrzenUkladuSlonecznego_Paint);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer bs_ZegarUkladuSlonecznego;
+    }
+}
+
